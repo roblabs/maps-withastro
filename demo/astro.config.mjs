e57e9b070f11193e3e3cc88cea30d22aa1d166eb
RobLabs.com <roblabs@users.noreply.github.com>
2024-04-16 10:21:49 -0700

    maps-withastro.roblabs.com

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -3,6 +3,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maps-withastro.roblabs.com',
   server: {
     port: 3141 // Gwynn
   },
